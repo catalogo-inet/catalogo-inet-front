3bb3c08e07a069cb86d1da7136cae0f686b62ad6
d4rm5 <dantemartinez06@gmail.com>
2023-10-19 22:55:23 -0300

    added cross svg

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import { StepOne, StepTwo, StepThree } from "./Steps";
+import { Cross } from "@/assets/icons/SvgIcons";
 
 export const Popup = () => {
   const [steps, setSteps] = useState(1);
   return steps <= 3 ? (
-    <div className="text-black rounded-xl flex flex-col shadow-md bg-white max-w-md">
+    <div className="p-5 flex justify-between w-[50vw] h-[70vh] text-black rounded-xl shadow-md bg-white items-center gap-4">
+      <button
+        onClick={() => {
+          setSteps(4);
+        }}
+        className="relative w-[57px] h-[57px]"
+      >
+        <Cross className={{ height: 20, width: 20 }} />
+      </button>
+
       {steps === 1 && <StepOne setSteps={setSteps} />}
       {steps === 2 && <StepTwo setSteps={setSteps} />}
       {steps === 3 && <StepThree setSteps={setSteps} />}
