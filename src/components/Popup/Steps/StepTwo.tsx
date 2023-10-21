b7ea553e12a2f7585fcf0dbd5085510ca6f27749
Juan Peñalba <juantandil123@gmail.com>
2023-10-21 12:47:05 -0300

    steptwo estilado

--- src/components/Popup/Steps/StepTwo.tsx
+++ src/components/Popup/Steps/StepTwo.tsx
@@ -15,28 +15,31 @@ export const StepTwo = ({ setSteps }) => {
       <Label className="text-black font-bold text-2xl mb-5">
         ¿Qué orientacion queres estudiar?
       </Label>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select a fruit" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Selecciona una orientación</SelectLabel>
-            <SelectItem value="apple">Programación</SelectItem>
-            <SelectItem value="banana">Informática</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <button
-        onClick={() => {
-          setSteps(3);
-        }}
-        type="submit"
-      >
-        Submit
-      </button>
-      <div>
-        <span>Todavía no lo decidí.</span>
+      <div className="mt-5 flex flex-col space-y-4 max-w-xs">
+        <Select>
+          <SelectTrigger className=" rounded-[5px]">
+            <SelectValue placeholder="Orientacion" />
+          </SelectTrigger>
+          <SelectContent className="bg-white">
+            <SelectGroup>
+              <SelectLabel>Selecciona una orientación</SelectLabel>
+              <SelectItem value="apple">Programación</SelectItem>
+              <SelectItem value="banana">Informática</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <button
+          onClick={() => {
+            setSteps(3);
+          }}
+          type="submit"
+          className="bg-[var(--color-blue)] text-white px-2 py-1 rounded-[5px]"
+        >
+          Continuar
+        </button>
+        <div>
+          <span>Todavía no lo decidí.</span>
+        </div>
       </div>
     </>
   );
