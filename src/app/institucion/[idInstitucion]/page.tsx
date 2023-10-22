d635bf036d8898a483d228037f71a03061871207
Juan Peñalba <juantandil123@gmail.com>
2023-10-22 15:06:58 -0300

    fetch a instituciones por id

--- src/app/institucion/[idInstitucion]/page.tsx
+++ src/app/institucion/[idInstitucion]/page.tsx
@@ -9,18 +9,17 @@ import {
 import { Layout } from "@/components/Layout";
 import { useFetch } from "@/hooks/useFetch";
 
-const Detail = ({ params }: { params: { direccion: string } }) => {
-  const { direccion } = params;
-  const direccionDecodificada = decodeURIComponent(direccion);
+const Detail = ({ params }: { params: { idInstitucion: number } }) => {
+  const { idInstitucion } = params;
   const { data, isLoading, hasError } = useFetch(
-    `http://localhost:7000/api/direcciones/${direccionDecodificada}`
+    `http://localhost:7000/api/instituciones/${idInstitucion}`
   );
 
   return (
     <Layout>
       <div className="mt-10">
         <h1>Escuela</h1>
-        <h2>{data && data[0].nombre}</h2>
+        <h2>{data && data[0].Nombre}</h2>
         <h2>Carreras disponibles</h2>
         <Accordion type="single" collapsible className="w-full">
           <AccordionItem value="item-1">
