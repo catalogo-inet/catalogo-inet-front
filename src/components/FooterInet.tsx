ff348b28fa991ef24ae0c3bfedf1238f687c40a5
d4rm5 <dantemartinez06@gmail.com>
2023-10-29 18:58:01 -0300

    hola

--- src/components/FooterInet.tsx
+++ src/components/FooterInet.tsx
@@ -4,210 +4,52 @@ import Image from "next/image";
 
 export function FooterInet() {
   return (
-    <footer className="py-2 px-5 bg-[var(--color-blue)] text-white">
-      <div className="container columns-6">
-        <div className="row">
-          <div className="col-sm-2 text-center">
-            <Image src={logoInet} alt="Logo Inet" width={200} />
-            <hr />
-            <p className="text-center">
-              Saavedra 789 - C1229ACE
-              <br />
-              Tel. (+54) 11 4129-2000
-            </p>
-            <hr />
-            <h4>Redes Sociales</h4>
-            <a
-              href="http://www.facebook.com/INET.ETP"
-              className="social"
-              target="_blank"
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+      <aside>
+        <p>
+          Instituto Nacional de Educación Técnica.
+          <br />
+          Saavedra 789 - C1229ACE Tel. (+54) 11 4129-2000
+        </p>
+      </aside>
+      <nav>
+        <header className="footer-title">Social</header>
+        <div className="grid grid-flow-col gap-4">
+          <a href="https://twitter.com/INETetp">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
             >
-              <i className="fa fa-2x fa-facebook"></i>
-            </a>{" "}
-            <a
-              href="http://twitter.com/INETetp"
-              target="_blank"
-              className="social"
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+            </svg>
+          </a>
+          <a href="https://www.youtube.com/channel/UCKpd-B8McDUTwc_3kbZm18g">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
             >
-              <i className="fa fa-2x fa-twitter"></i>
-            </a>{" "}
-            <a
-              href="http://www.youtube.com/channel/UCKpd-B8McDUTwc_3kbZm18g"
-              target="_blank"
-              className="social"
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+            </svg>
+          </a>
+          <a href="https://www.facebook.com/INET.ETP">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="fill-current"
             >
-              <i className="fa fa-2x fa-youtube-play"></i>
-            </a>{" "}
-            <a
-              href="http://www.instagram.com/inet_etp/"
-              target="_blank"
-              className="social"
-            >
-              <i className="fa fa-2x fa-instagram"></i>
-            </a>
-            <a href="https://www.tiktok.com/@inetetp" target="_blank">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="fa fa-2x"
-                height="1em"
-                viewBox="0 0 448 512"
-              >
-                <path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"></path>
-              </svg>
-            </a>
-            <hr />
-            <Image src={logoMinisterio} alt="Ministerio" width={250} />
-          </div>
-          <div className="col-xs-6 col-sm-2 col-sm-offset-1">
-            <h4>
-              <a href="http://www.inet.edu.ar/index.php/asuntos-federales/">
-                Asuntos Federales
-              </a>
-            </h4>
-            <ul className="list-unstyled">
-              <li>
-                <a href="http://www.inet.edu.ar/index.php/asuntos-federales/comision-federal/">
-                  Comisión Federal
-                </a>
-              </li>
-              <li>
-                <a href="http://www.inet.edu.ar/index.php/asuntos-federales/planes-de-mejora-de-la-etp/">
-                  Planes de Mejora
-                </a>
-              </li>
-              <li>
-                <a href="http://www.inet.edu.ar/index.php/asuntos-federales/programa-clan-capacitacion-laboral-de-alcance-nacional/">
-                  Programa CLAN
-                </a>
-              </li>
-              <li>
-                <a href="http://www.inet.edu.ar/index.php/asuntos-federales/infraestructura/">
-                  Infraestructura
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <div>
-              <div>
-                <h4>
-                  <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-secundaria-tecnica/">
-                    Secundario Técnico
-                  </a>
-                </h4>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-secundaria-tecnica/practicas-profesionalizantes/">
-                      Prácticas Profesionalizantes
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-secundaria-tecnica/olimpiadas-nacionales-de-construcciones-y-electromecanica/">
-                      Encuentros Educativos
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-secundaria-tecnica/marcos-de-referencia-de-nivel-secundario/">
-                      Marcos de Referencia
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-secundaria-tecnica/formacion-docente-inicial/">
-                      Formación Docente Inicial
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-secundaria-tecnica/finestec/">
-                      FinEsTec
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-xs-6 col-sm-4">
-                <h4>
-                  <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-superior-tecnica/">
-                    Educación Superior
-                  </a>
-                </h4>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/educacion-superior-tecnica/marcos-de-referencia-de-nivel-superior/">
-                      Marcos de Referencia
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-xs-6 col-sm-4">
-                <h4>
-                  <a href="http://www.inet.edu.ar/index.php/niveles-educativos/formacion-profesional/">
-                    Formación Profesional
-                  </a>
-                </h4>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/formacion-profesional/cursos-de-capacitacion/">
-                      Cursos de Capacitación
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/formacion-profesional/pretis/">
-                      Construir Inclusión
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/formacion-profesional/programa-federal-de-asistencia-tecnica-institucional-y-jurisdiccional/">
-                      Asistencia Técnica
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/formacion-profesional/marcos-de-referencia-de-formacion-profesional/">
-                      Marcos de Referencia
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/formacion-profesional/mesa-federal-de-trabajo-permanente-para-la-formacion-profesional/">
-                      Mesa Federal
-                    </a>
-                  </li>
-                  <li>
-                    <a href="http://www.inet.edu.ar/index.php/niveles-educativos/formacion-profesional/programa-federal-de-aulas-tecnicas-moviles/">
-                      Aulas Talleres Móviles
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="color-white">
-            <h4 className="text-2xl">
-              <a href="http://www.inet.edu.ar/index.php/normativa/">
-                Normativa
-              </a>
-            </h4>
-            <h4>
-              <a href="http://www.inet.edu.ar/index.php/institucional/conetyp/">
-                CoNETyP
-              </a>
-            </h4>
-            <h4>
-              <a href="http://www.inet.edu.ar/index.php/institucional/credito-fiscal/">
-                Crédito Fiscal
-              </a>
-            </h4>
-            <h4>
-              <a href="http://www.inet.edu.ar/index.php/material-de-capacitacion/">
-                Biblioteca
-              </a>
-            </h4>
-            <h4>
-              <a href="http://catalogo-inet.educacion.gob.ar/">
-                Catálogo de Títulos
-              </a>
-            </h4>
-          </div>
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
+          </a>
         </div>
-      </div>
+      </nav>
     </footer>
   );
 }
