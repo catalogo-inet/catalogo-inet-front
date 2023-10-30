cb1a48a41b4411ab7916a72e05ed2bbda976c56b
Dalviux9th <enzod3339@gmail.com>
2023-10-29 21:17:47 -0300

    minimapa mejorado

--- src/components/Minimap.tsx
+++ src/components/Minimap.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import { Marker, Popup, Tooltip } from "react-leaflet";
+import { Marker, AttributionControl, ScaleControl, ZoomControl, MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { customIcon } from "@/components/mapComponents/Capas";
-import { Controladores } from "./mapComponents/Controladores";
 import L from "leaflet";
 
 export function Minimap({ lat, lon }: { lat: number; lon: number }) {
   const center = L.latLng(lat, lon);
-
-  function FlyToCenter() {
-    const map = useMap();
-    map.flyTo(center, 17);
-  }
 
   return (
     <MapContainer
@@ -22,7 +14,7 @@ export function Minimap({ lat, lon }: { lat: number; lon: number }) {
       center={center}
       minZoom={5}
       maxZoom={18}
-      zoom={1}
+      zoom={15}
       zoomControl={false}
       attributionControl={false}
       boxZoom={false}
@@ -32,14 +24,21 @@ export function Minimap({ lat, lon }: { lat: number; lon: number }) {
       touchZoom={true}
     >
       <TileLayer
-        attribution='<a target="_blank" href="https://leafletjs.com/">Leaflet</a> | &copy; IGN - <a target="_blank" href="https://mapa.ign.gob.ar/">Argenmap</a> | &copy; <a target="_blank" href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution='<a target="_blank" href="https://leafletjs.com/">Leaflet</a> | &copy;<a target="_blank" href="https://mapa.ign.gob.ar/">Argenmap</a> | &copy; <a target="_blank" href="http://www.openstreetmap.org/copyright">OSM</a>'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       {/* Capa base Argenmap */}
       <TileLayer url="https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png" />
 
-      <Controladores />
+      {/* Controles UI */}
+      <ZoomControl
+        position="bottomleft"
+        zoomOutTitle={"Alejar"}
+        zoomInTitle={"Acercar"}
+      />
+
+      <AttributionControl position="topright" prefix={false} />
 
       <Marker position={center} icon={customIcon}></Marker>
     </MapContainer>
